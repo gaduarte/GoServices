cd8a90517ecfa138fc7a9bdaaeac6e2eac6c73ca
Gabi <93677092+gaduarte@users.noreply.github.com>
2024-02-10 15:31:23 -0300

    Update firebase.js

--- src/backend/Firebase/firebase.js
+++ src/backend/Firebase/firebase.js
@@ -4,14 +4,7 @@ import { getDatabase } from "firebase/database";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAjWrDAR_DACdqhq2P7nfnYI4H6M0YkX50",
-    authDomain: "goservices-a0bf9.firebaseapp.com",
-    databaseURL: "https://goservices-a0bf9-default-rtdb.firebaseio.com",
-    projectId: "goservices-a0bf9",
-    storageBucket: "goservices-a0bf9.appspot.com",
-    messagingSenderId: "966186778726",
-    appId: "1:966186778726:web:31e6300c46c447d03cada7",
-    measurementId: "G-H7L211LBSZ"
+    
 };
 
 const appF = initializeApp(firebaseConfig);
